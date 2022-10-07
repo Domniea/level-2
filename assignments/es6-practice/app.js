a30d90daca209ec9d9fad421cab457c01a5a54de
domniea <domniea@gmail.com>
2022-10-07 10:36:32 -0600

    Post Zen Garden

---
+++ assignments/es6-practice/app.js
@@ -0,0 +1,35 @@
+const name = "John"
+const age = 101
+
+function runForLoop(pets) {
+    const petObjects = []
+    for (let i = 0; i < pets.length; i++) {
+        const pet = { type: pets[i] }
+        let name;
+        if (pets[i] === "cat") {
+            name = "fluffy"
+        } else {
+            name = "spot"
+        }
+        console.log("pet name: ", name)
+        pet.name = name
+        petObjects.push(pet)
+    }
+    console.log("man name: ", name)
+    return petObjects
+}
+
+runForLoop(["cat", "dog"])
+
+// Re-write this `.map()` using an arrow function:
+
+// Be aware that if JavaScript sees a `{` directly after the `=>` it will think
+// it's starting a function, and not starting an object, so the `:` will be an unexpected symbol.
+
+const carrots = ["bright orange", "ripe", "rotten"]
+
+function mapVegetables(arr) {
+    return arr.map(function(carrot) {
+        return { type: "carrot", name: carrot }
+    })
+}
